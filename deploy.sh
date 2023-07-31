6a79ca4545a9e681d4997a2d9ec06416ba8c6da5
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2023-07-31 10:25:59 +0100

    chore: remove unneeded aspects from .deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ GREEN="\033[1;32m"
 NOCOLOR="\033[0m"
 
 stack_name="$1"
-audit_event_name_prefix="$2"
-cri_identifier="$3"
+
 
 if [ -z "$stack_name" ]
 then
@@ -15,25 +14,12 @@ echo -e "😱 ${RED}stack name expected as first argument, e.g. ${GREEN}./deploy
 exit 1
 fi
 
-if [ -z "$audit_event_name_prefix" ]
-then
-  audit_event_name_prefix="/common-cri-parameters/AuditEventNamePrefix"
-fi
-
-if [ -z "$cri_identifier" ]
-then
-  cri_identifier="/common-cri-parameters/CriIdentifier"
-fi
-
 
 echo -e "👉 deploying common lambdas with:"
 echo -e "\tstack name: ${GREEN}$stack_name${NOCOLOR}"
-echo -e "\tAuditEventNamePrefix SSM key ${GREEN}$audit_event_name_prefix${NOCOLOR}"
-echo -e "\tCriIdentifier SSM key ${GREEN}$cri_identifier${NOCOLOR}"
 
-./gradlew clean
-sam validate -t infrastructure/lambda/template.yaml
-sam build -t infrastructure/lambda/template.yaml
+sam validate -t infrastructure/template.yaml
+sam build -t infrastructure/template.yaml
 sam deploy --stack-name "$stack_name" \
    --no-fail-on-empty-changeset \
    --no-confirm-changeset \
@@ -43,5 +29,3 @@ sam deploy --stack-name "$stack_name" \
    --parameter-overrides \
    CodeSigningEnabled=false \
    Environment=dev \
-   AuditEventNamePrefix=$audit_event_name_prefix \
-   CriIdentifier=$cri_identifier
