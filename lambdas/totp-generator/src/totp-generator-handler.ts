46106657d0b5f03979bf27be2d1ec880acb58c97
Connor Johnson <connor.johnson@digital.cabinet-office.gov.uk>
2023-08-02 15:22:06 +0100

    chore: Replaced TOTP lib, and fixed tests.

---
+++ lambdas/totp-generator/src/totp-generator-handler.ts
@@ -0,0 +1,40 @@
+import { LambdaInterface } from "@aws-lambda-powertools/commons";
+
+import { generateConfig, Totp } from "time2fa";
+import { Logger } from "@aws-lambda-powertools/logger";
+const TOTP_TTL_IN_SECONDS = 30;
+const TOTP_HASH = "sha512";
+const TOTP_LENGTH = 8;
+
+const logger = new Logger();
+
+export class TotpGeneratorHandler implements LambdaInterface {
+  public async handler(
+    event: any,
+    _context: unknown
+  ): Promise<{ totp: string }> {
+    try {
+      if (!event.SecretString) {
+        throw new Error("No secret string present.");
+      }
+      const totp_config = generateConfig({
+        digits: TOTP_LENGTH,
+        period: TOTP_TTL_IN_SECONDS,
+        algo: TOTP_HASH,
+      });
+      const totp_code = Totp.generatePasscodes(
+        { secret: event.SecretString },
+        totp_config
+      )[0];
+      return {
+        totp: totp_code,
+      };
+    } catch (error: any) {
+      logger.error("Error TOTP Generator: " + error.message);
+      throw error;
+    }
+  }
+}
+
+const handlerClass = new TotpGeneratorHandler();
+export const lambdaHandler = handlerClass.handler.bind(handlerClass);
