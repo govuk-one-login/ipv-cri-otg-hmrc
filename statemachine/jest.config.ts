5d40df44cf74f565de2b49d08645678013794da3
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2023-08-09 09:17:04 +0100

    OJ-1770: Add ts configuration

---
+++ statemachine/jest.config.ts
@@ -0,0 +1,29 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+import type { Config } from "@jest/types";
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["statemachine/**/*.{js,ts}", "!**/tests/**"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coveragePathIgnorePatterns: ["config.ts", "node_modules/"],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  testMatch: ["**/tests/**/*.test.ts"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+};
+export default config;
