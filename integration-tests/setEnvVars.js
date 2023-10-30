82c4dfc3bc510fba5b908d50b6e43995d54d5f8c
Rahul Gupta <rahul.gupta@digital.cabinet-office.gov.uk>
2023-10-30 11:07:14 +0000

    OJ-1995 added GHA , modified stack name and state machine name

--- integration-tests/setEnvVars.js
+++ integration-tests/setEnvVars.js
@@ -6,9 +6,6 @@ process.env.AWS_SECRET_ACCESS_KEY = "local"; //pragma: allowlist secret
 process.env.AWS_SESSION_TOKEN = "local";
 
 process.env.STATE_MACHINE_ARN =
-  "arn:aws:states:eu-west-2:404250751813:stateMachine:pdv-matching-NinoCheck";
+  "arn:aws:states:eu-west-2:404250751813:stateMachine:oauth-token-generator";
 process.env.STATE_MACHINE_ROLE =
-  "arn:aws:iam::404250751813:role/pdv-matching-NinoCheckStateMachineRole-F6BSBWOOSN15";
-
-process.env.NINO_ATTEMPTS_TABLE = "pdv-matching-nino-attempts";
-process.env.NINO_USERS_TABLE = "pdv-matching-nino-users";
+  "arn:aws:iam::404250751813:role/oauth-token-generatorStateMachineRole-F6BSBWOOSN15";
