cda7bbadce934e9dcfbe198c1029968da2275cc2
Karol C. Gancarz <karol.sinx@gmail.com>
2024-02-26 11:04:25 +0000

    BAU: Increase the unhappy test timeout to prevent failure in GitHub Actions
    Rename the happy test to reflect what it does (looks like bad copy/paste).

--- integration-tests/tests/mocked/oauth-token-generator/oauth-token-generator-happy.test.ts
+++ integration-tests/tests/mocked/oauth-token-generator/oauth-token-generator-happy.test.ts
@@ -3,7 +3,7 @@ import { SfnContainerHelper } from "./sfn-container-helper";
 
 jest.setTimeout(30_000);
 
-describe("generate-totp-unhappy", () => {
+describe("generate-totp-happy", () => {
   let sfnContainer: SfnContainerHelper;
 
   beforeAll(async () => {
