e42fefba2b94fec0196afb071bf1cfd31f6a6584
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-04-05 08:36:30 +0100

    OJ-2391: Simulate retries and final passing test
    Added a test to show initial failures that can occur
    and subsequent passing due to recovery. See MockConfigFile
    for HappyOnLastTry, which illustrates the behaviour. This
    jest test makes this scenario clearer

--- integration-tests/tests/mocked/oauth-token-generator/oauth-token-generator-happy.test.ts
+++ integration-tests/tests/mocked/oauth-token-generator/oauth-token-generator-happy.test.ts
@@ -1,7 +1,7 @@
 import { HistoryEvent } from "@aws-sdk/client-sfn";
 import { SfnContainerHelper } from "./sfn-container-helper";
 
-jest.setTimeout(40_000);
+jest.setTimeout(60_000);
 
 describe("oauth-token-generator-happy.test", () => {
   let sfnContainer: SfnContainerHelper;
@@ -28,6 +28,38 @@ describe("oauth-token-generator-happy.test", () => {
       responseStepFunction
     );
     expect(results).toBeDefined();
+    expect(results[0].stateExitedEventDetails?.output).toBe("[{}]");
+  });
+
+  it("should succeed on final attempt, after multiple lambda errors retries", async () => {
+    const input = JSON.stringify({ tokenType: "stub" });
+    const responseStepFunction = await sfnContainer.startStepFunctionExecution(
+      "HappyOnLastTry",
+      input
+    );
+    const lambdaError = await sfnContainer.waitFor(
+      (event: HistoryEvent) =>
+        event?.type === "TaskFailed" &&
+        event?.taskFailedEventDetails?.resourceType == "lambda",
+      responseStepFunction
+    );
+    const results = await sfnContainer.waitFor(
+      (event: HistoryEvent) =>
+        event?.stateExitedEventDetails?.name === "Success",
+      responseStepFunction
+    );
+
+    expect(lambdaError).toHaveLength(3);
+    expect(lambdaError[0]?.taskFailedEventDetails?.cause).toBe(
+      "Lambda resource is not ready."
+    );
+    expect(lambdaError[1]?.taskFailedEventDetails?.cause).toBe(
+      "Lambda timed out."
+    );
+    expect(lambdaError[2]?.taskFailedEventDetails?.cause).toBe(
+      "Lambda timed out."
+    );
+
     expect(results[0].stateExitedEventDetails?.output).toBe("[{}]");
   });
 });
