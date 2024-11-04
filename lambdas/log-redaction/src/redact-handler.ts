16629e2c4da466808d5825ec65011390121cca0d
Suraj Kumar <sk96.uk@gmail.com>
2024-11-04 12:59:51 +0000

    OJ-2897: Update redaction-handler to check if log stream exists before creating it

--- lambdas/log-redaction/src/redact-handler.ts
+++ lambdas/log-redaction/src/redact-handler.ts
@@ -9,11 +9,24 @@ import { LambdaInterface } from "@aws-lambda-powertools/commons";
 import { CloudWatchLogsDecodedData, CloudWatchLogsEvent } from "aws-lambda";
 import zlib from "zlib";
 import { redact } from "./redactor";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  QueryCommand,
+} from "@aws-sdk/client-dynamodb";
 
 const logger = new Logger();
 const cloudwatch = new CloudWatchLogsClient();
 
+const logStreamTrackingTable = process.env.LogStreamTrackingTbl;
+
 export class RedactHandler implements LambdaInterface {
+  private readonly dynamodb: DynamoDBClient;
+
+  constructor(dynamodb = new DynamoDBClient()) {
+    this.dynamodb = dynamodb;
+  }
+
   public async handler(
     event: CloudWatchLogsEvent,
     _context: unknown
@@ -28,49 +41,13 @@ export class RedactHandler implements LambdaInterface {
       const redactLogGroup = logEvents.logGroup + "-redacted";
       const logStream = logEvents.logStream;
 
-      try {
-        await cloudwatch.send(
-          new CreateLogStreamCommand({
-            logGroupName: redactLogGroup,
-            logStreamName: logStream,
-          })
-        );
-      } catch (error: unknown) {
-        const message = error instanceof Error ? error.message : String(error);
-        if (!message.includes("The specified log stream already exists")) {
-          throw error;
-        }
-        logger.info(logStream + " already exists");
-      }
+      await this.createLogStream(logStream, redactLogGroup);
 
       for (const logEvent of logEvents.logEvents) {
-        const original = logEvent.message;
         logEvent.message = redact(logEvent.message);
-        logger.info("Updated message: " + original + " to " + logEvent.message);
       }
 
-      logger.info("Putting redacted logs into " + redactLogGroup);
-
-      try {
-        const response: PutLogEventsCommandOutput = await cloudwatch.send(
-          new PutLogEventsCommand({
-            logGroupName: redactLogGroup,
-            logStreamName: logStream,
-            logEvents: logEvents.logEvents.map((event) => ({
-              id: event.id,
-              message: this.formatMessage(event.message),
-              timestamp: event.timestamp,
-              extractedFields: event.extractedFields,
-            })),
-          })
-        );
-        logger.info(JSON.stringify(response));
-      } catch (error) {
-        logger.error(
-          `Error putting log events into ${redactLogGroup}: ${error}`
-        );
-        throw error;
-      }
+      await this.saveRedactedLog(redactLogGroup, logStream, logEvents);
 
       return {};
     } catch (error: unknown) {
@@ -78,6 +55,81 @@ export class RedactHandler implements LambdaInterface {
       logger.error(`Error in RedactHandler: ${message}`);
       throw error;
     }
+  }
+
+  private async saveRedactedLog(
+    redactLogGroup: string,
+    logStream: string,
+    logEvents: CloudWatchLogsDecodedData
+  ) {
+    logger.info("Putting redacted logs into " + redactLogGroup);
+
+    try {
+      const response: PutLogEventsCommandOutput = await cloudwatch.send(
+        new PutLogEventsCommand({
+          logGroupName: redactLogGroup,
+          logStreamName: logStream,
+          logEvents: logEvents.logEvents.map((event) => ({
+            id: event.id,
+            message: this.formatMessage(event.message),
+            timestamp: event.timestamp,
+            extractedFields: event.extractedFields,
+          })),
+        })
+      );
+      logger.info(JSON.stringify(response));
+    } catch (error) {
+      logger.error(`Error putting log events into ${redactLogGroup}: ${error}`);
+      throw error;
+    }
+  }
+
+  private async createLogStream(logStreamName: string, logGroupName: string) {
+    const query = await this.queryTable(logStreamName);
+
+    if (query.Count == 0) {
+      logger.info("Creating log stream " + logStreamName);
+
+      await cloudwatch.send(
+        new CreateLogStreamCommand({
+          logGroupName: logGroupName,
+          logStreamName: logStreamName,
+        })
+      );
+
+      await this.createLogStreamRecordInDB(logStreamName);
+    }
+  }
+
+  private async createLogStreamRecordInDB(logStream: string) {
+    await this.dynamodb.send(
+      new PutItemCommand({
+        TableName: logStreamTrackingTable,
+        Item: {
+          logStreamName: {
+            S: logStream,
+          },
+          expiryDate: {
+            N: (Math.floor(Date.now() / 1000) + 120 * 60).toString(),
+          },
+        },
+      })
+    );
+    logger.info("Added " + logStream + " to " + logStreamTrackingTable);
+  }
+
+  private async queryTable(logStream: string) {
+    return await this.dynamodb.send(
+      new QueryCommand({
+        TableName: logStreamTrackingTable,
+        KeyConditionExpression: "logStreamName = :logStream",
+        ExpressionAttributeValues: {
+          ":logStream": {
+            S: logStream,
+          },
+        },
+      })
+    );
   }
 
   private formatMessage(message: string) {
